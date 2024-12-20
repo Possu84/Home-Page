8852b4caf0120f9162410b4ff7ce6f4c3e277c68
Possu84 <pooters.jonas@gmail.com>
2024-12-20 16:23:27 +0200

    Added Bio and updated routing

---
+++ src/components/Bio.js
@@ -0,0 +1,22 @@
+import React from "react"; 
+
+const Bio = () =>{
+
+    return(
+        <div>
+            <h1>
+
+                BIO
+            </h1>
+
+
+
+        </div>
+
+
+
+
+    );
+};
+
+export default Bio;
